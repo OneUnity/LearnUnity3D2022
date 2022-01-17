2aaee92312f87803f4cb2e40ea4bc974a6aae6b6
OneUnity <wonjin9952@naver.com>
2022-01-17 13:07:09 +0900

    HelloWorld

---
+++ HelloWorld/Assets/Scripts/App.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class App : MonoBehaviour
+{
+    int num = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+        Debug.Log(num);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
